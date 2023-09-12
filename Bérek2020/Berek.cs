941a1159dd3e283c497389f80ffaa8ee26164d6e
zeteny12 <zetus0512@gmail.com>
2023-09-12 14:22:46 +0200

    Add project files.

---
+++ Bérek2020/Berek.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bérek2020
+{
+    internal class Berek
+    {
+        string Nev;
+        string Neme;
+        string Reszleg;
+        int Belepes;
+        int Ber;
+
+        public string Nev1 { get => Nev; set => Nev = value; }
+        public string Neme1 { get => Neme; set => Neme = value; }
+        public string Reszleg1 { get => Reszleg; set => Reszleg = value; }
+        public int Belepes1 { get => Belepes; set => Belepes = value; }
+        public int Ber1 { get => Ber; set => Ber = value; }
+
+        public Berek(string nev1, string neme1, string reszleg1, int belepes1, int ber1)
+        {
+            Nev1 = nev1;
+            Neme1 = neme1;
+            Reszleg1 = reszleg1;
+            Belepes1 = belepes1;
+            Ber1 = ber1;
+        }
+
+        public Berek(string sor)
+        {
+            string[] darab = sor.Split(';');
+            Nev = darab[0];
+            Neme = darab[1];
+            Reszleg = darab[2];
+            Belepes = int.Parse(darab[3]);
+            Ber = int.Parse(darab[4]);
+        }
+    }
+}
---
+++ Bérek2020/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bérek2020
+{
+    internal class Program
+    {
+
+        static List<Berek> lista = new List<Berek>();
+
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+            Feladat7();
+
+            Console.ReadKey();
+        }
+
+        private static void Feladat7()
+        {
+            Console.WriteLine("7. feladat: Statisztika");
+            foreach (var item in lista.GroupBy(a => a.Reszleg1).Select(b => new {reszleg = b.Key, darab=b.Count()}))
+            {
+                Console.WriteLine("\t" + item.reszleg + " - " + item.darab + " fő");
+            }
+        }
+
+        private static void Feladat5()
+        {
+            Console.Write("5. feladat: Kérem egy részleg nevét: ");
+            string ReszlegNeve = Console.ReadLine();
+            Console.Write("6. feladat: ");
+        }
+
+        private static void Feladat4()
+        {
+            int ossz = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                ossz = ossz + lista[i].Ber1;
+            }
+            double atl = ossz / lista.Count;
+            Console.WriteLine("4. feladat: Bérek átlaga: " + atl + " eFt");
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine("3. feladat: Dolgozók száma: " + lista.Count + " fő");
+        }
+
+        private static void Beolvasas()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("berek2020.txt"))
+                {
+                    sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        lista.Add(new Berek(sr.ReadLine()));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
